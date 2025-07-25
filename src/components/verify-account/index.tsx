5363d15ed3be32d34dad6a40469d9ade14a94f57
azambembem <azamjan980713@gmail.com>
2025-07-26 01:46:05 +0900

    varify-account design

--- src/components/verify-account/index.tsx
+++ src/components/verify-account/index.tsx
@@ -6,7 +6,7 @@ export const VerifyAccount = () => {
   const queryParams = new URLSearchParams(location.search);
   const email = queryParams.get("email");
   const {
-    onResendEmail: { mutate: onResendEmail },
+    onResendEmail: { mutate: onResendEmail }
   } = useVerifyAccountFeatures();
 
   if (!email) {
@@ -14,12 +14,12 @@ export const VerifyAccount = () => {
   }
 
   return (
-    <div className="min-h-screen bg-[#262626] flex items-start justify-center pt-[152px] p-4">
-      <div className="w-full max-w-[90vw] sm:max-w-md lg:max-w-lg bg-transparent sm:bg-[#2c2c2c] rounded-none md:rounded-[12px] p-4 sm:p-6 lg:p-12">
+    <div className="min-h-screen  bg-gradient-to-l from-[#4BC0C8] via-[#C779D0] to-[#FEAC5E] flex items-start justify-center pt-[152px] p-4">
+      <div className="w-full max-w-[90vw] sm:max-w-md lg:max-w-lg bg-transparent sm:bg- bg-gradient-to-l from-[#4BC0C8] via-[#C779D0] to-[#FEAC5E] rounded-none md:rounded-[12px] p-4 sm:p-6 lg:p-12">
         <h1 className="text-[#EEEEEE] text-[16px] sm:text-[20px] md:text-[20px] lg:text-[24px] font-semibold text-center mb-6 sm:mb-8 lg:mb-12">
           인증 메일 발송
         </h1>
-        <p className="text-[#a5a5a5] text-[13px] md:text-[14px] lg:text-[14px] text-center mb-6 sm:mb-8 font-light">
+        <p className="text-[#eee] text-[13px] md:text-[14px] lg:text-[14px] text-center mb-6 sm:mb-8 font-light">
           이메일을 확인하여 계정 인증을 진행해 주세요.
           <br /> 이메일 내 인증 링크는 60분 후 만료됩니다.
         </p>
