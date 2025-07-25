0d580d76b0242ec37a65cf3619c8ae4adc7b7832
azambembem <azamjan980713@gmail.com>
2025-07-26 01:19:38 +0900

    login pages design

--- src/components/sign-in/index.tsx
+++ src/components/sign-in/index.tsx
@@ -26,8 +26,8 @@ const SignIn = () => {
   });
 
   return (
-    <div className="min-h-screen bg-[#262626] flex items-start justify-center pt-[152px] p-4">
-      <div className="w-full max-w-[90vw] sm:max-w-md lg:max-w-lg bg-transparent sm:bg-[#2c2c2c] rounded-none md:rounded-[12px] p-4 sm:p-6 lg:p-12">
+    <div className="min-h-screen  bg-gradient-to-l from-[#4BC0C8] via-[#C779D0] to-[#FEAC5E] flex items-start justify-center pt-[152px] p-4">
+      <div className="w-full max-w-[90vw] sm:max-w-md lg:max-w-lg bg-transparent sm: bg-gradient-to-l from-[#4BC0C8] via-[#C779D0] to-[#FEAC5E] rounded-none md:rounded-[12px] p-4 sm:p-6 lg:p-12">
         <h1 className="text-[#EEEEEE] text-[16px] md:text-[20px] lg:text-[24px] font-semibold text-center mb-6 sm:mb-8 lg:mb-12">
           로그인
         </h1>
@@ -52,7 +52,7 @@ const SignIn = () => {
               value={formik.values.email}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              className="bg-[#2B2B2B] border-[#4F4F4F] text-[#EEEEEE] placeholder-[#747474] h-[42px] text-[14px] font-light"
+              className=" border-[#eee] text-[#EEEEEE] placeholder-[#747474] h-[42px] text-[14px] font-light"
             />
             {formik.touched.email && formik.errors.email && (
               <p className="text-[#FF7B7B] text-[12px] mt-1">
@@ -76,7 +76,7 @@ const SignIn = () => {
               value={formik.values.password}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              className="bg-[#2B2B2B] border-[#4F4F4F] text-[#EEEEEE] placeholder-[#747474] h-[42px] text-[14px] font-light"
+              className=" border-[#eee] text-[#EEEEEE] placeholder-[#747474] h-[42px] text-[14px] font-light"
             />
             {formik.touched.password && formik.errors.password && (
               <p className="text-[#FF7B7B] text-[12px] mt-1">
@@ -98,14 +98,14 @@ const SignIn = () => {
               <button
                 onClick={() => navigate("/sign-up")}
                 type="button"
-                className="text-[#EEEEEE] hover:text-[#bbbaba] transition-colors"
+                className="text-[#EEEEEE] hover:text-[#333] transition-colors"
               >
                 회원가입
               </button>
-              <span className="text-[#4F4F4F]">|</span>
+              <span className="text-[#eee]">|</span>
               <button
                 type="button"
-                className="text-[#EEEEEE] hover:text-[#bbbaba] transition-colors"
+                className="text-[#EEEEEE] hover:text-[#333] transition-colors"
                 onClick={() => navigate("/forgot-password")}
               >
                 아이디/비밀번호 찾기
