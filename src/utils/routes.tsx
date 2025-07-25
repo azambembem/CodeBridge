6d1fd0df1c183e3cf8bc667a99c8fe352631411a
azambembem <azamjan980713@gmail.com>
2025-07-25 23:24:36 +0900

    Sign in utils

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -2,6 +2,7 @@ import { v4 } from "uuid";
 import type { TRoute } from "@/types/shared";
 import Home from "@/pages/home";
 import SignUp from "@/pages/sign-up";
+import SignIn from "@/pages/sign-in";
 
 export const authed_route: TRoute[] = [
   {
@@ -15,6 +16,11 @@ export const unauthed_route: TRoute[] = [
   {
     path: "/",
     element: <Home />,
+    id: v4()
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
     id: v4()
   },
   {
