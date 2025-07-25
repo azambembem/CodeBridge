8d694e7a936cbf8c6c26280497fc06d6860077e2
azambembem <azamjan980713@gmail.com>
2025-07-26 01:44:39 +0900

    account-verified

--- src/components/account-verified/index.tsx
+++ src/components/account-verified/index.tsx
@@ -19,12 +19,12 @@ export const AccountVerified = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen bg-[#262626] flex flex-col items-center px-4 pt-[140px] md:pt-[188px] lg:pt-[232px]">
+    <div className="w-full h-screen  bg-gradient-to-l from-[#4BC0C8] via-[#C779D0] to-[#FEAC5E] flex flex-col items-center px-4 pt-[140px] md:pt-[188px] lg:pt-[232px]">
       <div className="text-center max-w-md-full">
         <h1 className="text-[16px] md:text-[20px] lg:text-[24px] text-[#eee] mb-4 font-pre font-semibold">
           계정 인증이 완료되었습니다!
         </h1>
-        <p className="text-[#A5A5A5] text-[13px] md:text-[14px] lg:text-[16px] mb-8 font-pre font-light">
+        <p className="text-[#eee] text-[13px] md:text-[14px] lg:text-[16px] mb-8 font-pre font-light">
           이용 중이던 화면으로 돌아가 주세요.
         </p>
       </div>
